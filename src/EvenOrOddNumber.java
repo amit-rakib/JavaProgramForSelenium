428de2609d8219d6344639ef1be1792221264bff
amit <mr.ahr.es6@gmail.com>
2025-02-05 11:38:26 +0600

    Even or Odd Number code

---
+++ src/EvenOrOddNumber.java
@@ -0,0 +1,17 @@
+
+public class EvenOrOddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 11;
+		
+		if(number % 2 == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}
