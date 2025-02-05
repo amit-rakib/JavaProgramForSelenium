036ac7a30b53860f05019ca479f10035e815d181
amit <mr.ahr.es6@gmail.com>
2025-02-05 23:45:02 +0600

    Binary Search using method

---
+++ src/BinarySearchUsingMethod.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class BinarySearchUsingMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int array[] = {10, 20, 30, 40, 50 }; // should be in order
+		
+		System.out.println(Arrays.binarySearch(array, 30));
+
+	}
+
+}
