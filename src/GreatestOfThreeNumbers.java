de09618f131ec25a72cec573e8ccc27076a5afea
amit <mr.ahr.es6@gmail.com>
2025-02-06 21:47:55 +0600

    Greatest of three numbers

---
+++ src/GreatestOfThreeNumbers.java
@@ -0,0 +1,22 @@
+
+public class GreatestOfThreeNumbers {
+
+	public static void main(String[] args) {
+        
+		 int a, b, c;
+		 a=4;
+		 b=9;
+		 c=7;
+		 
+		 
+		 if(a>b && a>c) {
+			 System.out.println(a);
+		 }else if(b>a && b>c) {
+			 System.out.println(b);
+		 }else {
+			 System.out.println(c);
+		 }
+		
+	}
+
+}
