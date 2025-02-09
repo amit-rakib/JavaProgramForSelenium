a6affd51fa5c3b15353cbaf8e6b2f07ba3ac62bd
amit <mr.ahr.es6@gmail.com>
2025-02-09 20:27:11 +0600

    Multiplication Table

--- src/Test.java
+++ src/Test.java
@@ -3,65 +3,20 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		int array[] = { 200, 400, 300, 900, 500};
+		int num = 125;
 		
-		for(int i = 0; i<array.length; i++) {
-			for(int j=0; j<array.length-i-1; j++) {
-				
-				if(array[i]> array[i+1]) {
-					int swap = array[i];
-					array[i] = array[i+1];
-					array[i+1] = swap;
-				}
-			}
+		int a = num;
+	int	b = 0;
+		
+		while(num>0) {
+			int digit = num%10;
+			b = b*10+digit;
+			num = num/10;
 		}
 		
-		for(int i : array) {
-			System.out.println(i);
-
+		if(a == b) {
+			System.out.println("Plau");
 		}
-		
-		int search = 400;
-		int low = 0;
-		int high = array.length;
-		
-		while(low <= high) {
-			int mid = (low+high)/2;
-			
-			if(array[mid] == search) {
-				System.out.println(mid+1);
-				//return;
-				break;
-			} else if(array[mid] < search){
-				low = mid+1;
-			} else {
-				high = mid-1;
-			}
-			
-		}
-		int sea = 900;
-		
-		for(int i = 0; i<array.length; i++) {
-			if(array[i] == sea) {
-				System.out.println("Linear: "+ (i+1));
-			} 
-		}
-		
-		
-		String text  = "Hello world hello";
-	text =	text.trim();
-		int count = 1;
-		for(int i = 0; i< text.length(); i++) {
-			if(text.charAt(i) == ' ' && text.charAt(i+1) != ' ') {
-				count++;
-			}
-			
-			
-		}
-		
-		System.out.println(count);
-
-		
 	}
 
 }
