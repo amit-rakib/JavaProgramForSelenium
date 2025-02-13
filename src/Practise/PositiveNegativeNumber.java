2a08b0423de4e32fd58c9edbe3477e23587f402a
amit <mr.ahr.es6@gmail.com>
2025-02-14 01:41:59 +0600

    Positive and negitive number

--- src/Practise/PositiveNegativeNumber.java
+++ src/Practise/PositiveNegativeNumber.java
@@ -4,6 +4,16 @@ public class PositiveNegativeNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		int num = -12;
+		
+		if(num>0) {
+			System.out.println("Positive");
+		} else {
+			System.out.println("Negative");
+		}
+		
 
 	}
 
