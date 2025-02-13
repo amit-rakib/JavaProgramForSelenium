8536b6f5e5c9471398b13d09de5ea7757f9ac49b
amit <mr.ahr.es6@gmail.com>
2025-02-14 00:25:35 +0600

    If Else Condition class

---
+++ src/Practise/IfElseCondition.java
@@ -0,0 +1,19 @@
+package Practise;
+
+public class IfElseCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int age =  35;
+		
+		if( age > 18) {
+			System.out.println("Elligible for vote");
+		} else {
+			System.out.println("Not eligible for vote");
+		}
+
+	}
+
+}
