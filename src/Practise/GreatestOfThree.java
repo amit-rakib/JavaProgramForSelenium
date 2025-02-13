57eeb06e9315d1773b994d43b739c9fcd380d3c4
amit <mr.ahr.es6@gmail.com>
2025-02-14 00:23:15 +0600

    Greatest of three class added

---
+++ src/Practise/GreatestOfThree.java
@@ -0,0 +1,25 @@
+package Practise;
+
+public class GreatestOfThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				int  a = 45;
+				int b = 65;
+				int c = 90;
+				
+				
+				if(a> b && a> c) {
+					System.out.println(a);
+				} else if( b> a && b> c) {
+					System.out.println(b);
+				} else {
+					System.out.println(c);
+				}
+				
+		   
+
+	}
+
+}
