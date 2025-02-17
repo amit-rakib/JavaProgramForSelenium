bee3f76252db1ab6bc932de41ea1a4c97e20abc5
amit <mr.ahr.es6@gmail.com>
2025-02-17 16:21:13 +0600

    Pyramids

---
+++ src/Pyramid2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pyramid2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a num: ");
+		int rows = sc.nextInt();
+		
+		//int rows = 3;
+		
+		
+		for(int i=1; i<=rows; i++) {
+			
+			for(int j = 0; j<=rows-i; j++ ) {
+				System.out.print(" ");
+			}
+			
+			for(int k = 1; k<=(2*i-1); k++) {
+				System.out.print("*");
+			}
+			
+			
+			
+			System.out.println();
+		}
+	}
+	
+
+}
