515acb074cbb1739c1353c46c0cc941746a8b25e
amit <mr.ahr.es6@gmail.com>
2025-03-09 02:33:50 +0600

    Even Oe Odd Number

---
+++ src/NoteOfSelenium/EvenOrOddNumber.java
@@ -0,0 +1,19 @@
+package NoteOfSelenium;
+
+public class EvenOrOddNumber {
+
+	public static void main(String[] args) {
+            
+		
+		int num = 20; 
+		
+		
+		if(num%2==0) {
+			System.out.print("Even");
+		} else {
+			System.out.println("Odd");
+		}
+		
+	}
+
+}
