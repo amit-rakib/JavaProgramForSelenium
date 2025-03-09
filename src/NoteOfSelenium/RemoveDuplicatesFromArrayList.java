4726ad034da3dafd871d332fd56fc968984020bf
amit <mr.ahr.es6@gmail.com>
2025-03-10 01:15:58 +0600

    Remove Duplicates from ArrayList

---
+++ src/NoteOfSelenium/RemoveDuplicatesFromArrayList.java
@@ -0,0 +1,34 @@
+package NoteOfSelenium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class RemoveDuplicatesFromArrayList {
+
+	public static void main(String[] args) {
+           
+		ArrayList<String> ar = new ArrayList();
+		
+		ar.add("Java");
+		ar.add("Java");
+		ar.add("Hello");
+		ar.add("Amit");
+		
+		
+		
+		System.out.println(ar);
+		
+		
+		
+		
+		
+		HashSet<String> st = new HashSet<>(ar);
+		
+		ArrayList<String> newAr = new ArrayList<String>(st);
+		
+		
+		System.out.println(newAr);
+	}
+
+}
