44d6714a2e6ecc52cea42bea688a4cf9716cb23c
amit <mr.ahr.es6@gmail.com>
2025-03-10 01:56:15 +0600

    Reverse Chars

---
+++ src/NoteOfSelenium/ReverseChars.java
@@ -0,0 +1,21 @@
+package NoteOfSelenium;
+
+public class ReverseChars {
+
+	public static void main(String[] args) {
+          
+		String str = "Hello world";
+		
+        
+		String rev = "";
+		
+		for(int i = str.length()-1; i>=0; i-- ) {
+			
+			rev = rev + str.charAt(i);
+		}
+		
+		System.out.println(rev);
+		 
+	}
+
+}
