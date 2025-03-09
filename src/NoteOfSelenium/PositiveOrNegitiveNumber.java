7ee5392d19d5ca910f896debda0ae2a8856dfed0
amit <mr.ahr.es6@gmail.com>
2025-03-10 01:01:53 +0600

    Positive or Negitive Number

---
+++ src/NoteOfSelenium/PositiveOrNegitiveNumber.java
@@ -0,0 +1,16 @@
+package NoteOfSelenium;
+
+public class PositiveOrNegitiveNumber {
+
+	public static void main(String[] args) {
+         
+		int num = -19;
+		
+		if(num > 0) {
+			System.out.println("Its a Positive Number");
+		} else {
+			System.out.println("Its a Negitive Number");
+		}
+	}
+
+}
