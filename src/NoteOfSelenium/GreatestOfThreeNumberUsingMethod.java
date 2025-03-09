bf7dc767d2cfe706ac76bf533177fbe173cdb157
amit <mr.ahr.es6@gmail.com>
2025-03-09 12:16:14 +0600

    Greaest of three number using method

--- src/NoteOfSelenium/GreatestOfThreeNumberUsingMethod.java
+++ src/NoteOfSelenium/GreatestOfThreeNumberUsingMethod.java
@@ -3,8 +3,28 @@ package NoteOfSelenium;
 public class GreatestOfThreeNumberUsingMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+             
+		
+		int a, b, c;
+		
+		a = 200;
+		b = 300;
+		c = 100;
+		
+		int max = findGreatest(a, b, c);
+		
+		System.out.println(max);
+		
+	}
 
+	private static int findGreatest(int a, int b, int c) {
+        
+		int max1 = Math.max(a, b);
+		
+		int max2 = Math.max(max1, c);
+		
+		return max2;
+		
 	}
 
 }
