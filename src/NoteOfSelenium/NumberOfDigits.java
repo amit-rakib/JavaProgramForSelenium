3e3d2f01d84a457177a1b7b4e0bc2509db05ebbb
amit <mr.ahr.es6@gmail.com>
2025-03-10 00:04:02 +0600

    Number of Digit

---
+++ src/NoteOfSelenium/NumberOfDigits.java
@@ -0,0 +1,21 @@
+package NoteOfSelenium;
+
+public class NumberOfDigits {
+
+	public static void main(String[] args) {
+           
+		
+		
+		int num = 3240;
+		int count = 0;
+		
+		while(num != 0) {
+			
+			num = num/10;
+			count++;
+		}
+		
+		System.out.println(count);
+	}
+
+}
