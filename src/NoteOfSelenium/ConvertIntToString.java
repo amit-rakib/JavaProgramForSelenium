28781e9408235fdec11a81b3c5eaaaf4b5599380
amit <mr.ahr.es6@gmail.com>
2025-03-09 22:04:56 +0600

    Integer to String

---
+++ src/NoteOfSelenium/ConvertIntToString.java
@@ -0,0 +1,24 @@
+package NoteOfSelenium;
+
+public class ConvertIntToString {
+
+	public static void main(String[] args) {
+         
+		int i = 128;
+		
+		
+		
+		
+		
+		String str1 = Integer.toString(i);
+		
+		String str2 = String.valueOf(i);
+		
+		
+		System.out.println(str1);
+		System.out.println(str2);
+
+		
+	}
+
+}
