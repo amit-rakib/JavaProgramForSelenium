1d07454231e3dea3ba611a09568c7cc347071afc
amit <mr.ahr.es6@gmail.com>
2025-03-09 12:20:09 +0600

    if else condition

---
+++ src/NoteOfSelenium/IfElseCondition.java
@@ -0,0 +1,17 @@
+package NoteOfSelenium;
+
+public class IfElseCondition {
+
+	public static void main(String[] args) {
+         
+		
+		int age = 28;
+		
+		if(age >= 18) {
+			System.out.println("Can vote");
+		} else {
+			System.out.println("Cant vote");
+		}
+	}
+
+}
